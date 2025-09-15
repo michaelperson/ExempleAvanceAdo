f7afbd1aca705d863861bfa15c812811ebbe150f
michael person <michael.person@cognitic.be>
2025-09-15 12:04:38 +0200

    Changement des couleurs corporate

--- ExempleAvanceAdo/ExempleConsoleAdo/Program.cs
+++ ExempleAvanceAdo/ExempleConsoleAdo/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Magenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("Exemple ADO!");
 Console.WriteLine("Propulsed By Ousmane");
